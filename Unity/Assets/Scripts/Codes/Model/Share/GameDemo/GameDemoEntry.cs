ecb1a888ce5971fac998ccde2110bb95076c513a
LCM <1572789824@qq.com>
2023-05-15 22:09:17 +0800

    修改启动脚本

---
+++ Unity/Assets/Scripts/Codes/Model/Share/GameDemo/GameDemoEntry.cs
@@ -0,0 +1,25 @@
+﻿namespace ET.GameDemo
+{
+    public static class GameDemoEntry
+    {
+        public static void Start()
+        {
+            StartGameAsync().Coroutine();
+        }
+
+        private static async ETTask StartGameAsync()
+        {
+            WinPeriod.Init();
+            MongoHelper.Init();
+            ProtobufHelper.Init();
+
+            Game.AddSingleton<NetServices>();
+            Game.AddSingleton<Root>();
+            await Game.AddSingleton<ConfigComponent>().LoadAsync();
+
+            await EventSystem.Instance.PublishAsync(Root.Instance.Scene, new GameDemo.EventType.EntryGameDemoEvent_InitShare());
+            await EventSystem.Instance.PublishAsync(Root.Instance.Scene, new GameDemo.EventType.EntryGameDemoEvent_InitServer());
+            await EventSystem.Instance.PublishAsync(Root.Instance.Scene, new GameDemo.EventType.EntryGameDemoEvent_InitClient());
+        }
+    }
+}
