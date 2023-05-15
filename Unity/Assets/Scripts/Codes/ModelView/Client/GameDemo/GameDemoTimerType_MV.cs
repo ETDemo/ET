182f1c5124cb0b3cecdc4de7a9e1be10bd666b70
LCM <1572789824@qq.com>
2023-05-15 18:46:45 +0800

    黑屏逻辑

---
+++ Unity/Assets/Scripts/Codes/ModelView/Client/GameDemo/GameDemoTimerType_MV.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace ET.GameDemo
+{
+    [UniqueId(2000,3000)]
+    public static class GameDemoTimerType_MV
+    {
+        public const int ScreenFade = 2001;
+    }
+}
